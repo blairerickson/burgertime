0fe0aaf1b8063c494a4d428f1e9b74b76361a145
Blair Erickson <blair@jamwix.com>
2017-04-26 23:46:24 -0400

    Schema added

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM burgers_db.burgers
+LIMIT 0, 1000
+
+-- Date: 2017-04-26 23:42
+*/
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (1,'Steak Burger',1,'');
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (2,'BLT',0,'');
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (3,'Big Mac',0,'');
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (4,'Whopper',0,'');
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (5,'Big Kahuna',1,'');
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (6,'Western BBQ',0,'');
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`date`) VALUES (7,'Double Deluxe',1,'');
